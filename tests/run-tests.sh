61babdc2ff19eb170814b060e039b60815ac7d52
Mario Loriedo <mario.loriedo@gmail.com>
2024-01-23 16:12:38 +0100

    Fix UBI9 based image
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 CLUSTER_URL=${CLUSTER_URL:-"https://api.sandbox-m2.ll9k.p1.openshiftapps.com:6443"}
-VS_CODE_IMAGE=${VS_CODE_IMAGE:-"quay.io/che-incubator/che-code:insiders"}
+EDITOR_IMAGE=${EDITOR_IMAGE:-"quay.io/che-incubator/che-code:insiders"}
 
 echo ""
 echo "##############################################"
@@ -12,29 +12,13 @@ echo "##                                          ##"
 echo "##############################################"
 oc login --token "${CLUSTER_TOKEN}" --server="${CLUSTER_URL}"
 
-function cleanup {
-    echo ""
-    echo "##############################################"
-    echo "##                                          ##"
-    echo "##              DELETING DWT                ##"
-    echo "##                                          ##"
-    echo "##############################################"
-    kubectl delete dwt/editor
-}
-trap cleanup EXIT
-
-
 echo ""
 echo "##############################################"
 echo "##                                          ##"
-echo "##           CREATE EDITOR CR               ##"
+echo "##      RUNNING TESTS ON ALL IMAGES         ##"
 echo "##                                          ##"
 echo "##############################################"
-echo "VS_CODE_IMAGE: ${VS_CODE_IMAGE}"
-export VS_CODE_CONTRIB=$(cat ./tests/editor-contribution.yaml | sed "s|IMAGE_NAME|${VS_CODE_IMAGE}|g")
-kubectl apply -f - <<< "${VS_CODE_CONTRIB}"
-
- CLOUD_DEV_IMAGES_TAGS=( "ubi8"
+CLOUD_DEV_IMAGES_TAGS=( "ubi8"
                     "ubi9"
                    # "alpine"
                    # "busybox"
@@ -48,9 +32,12 @@ kubectl apply -f - <<< "${VS_CODE_CONTRIB}"
 for tag in "${CLOUD_DEV_IMAGES_TAGS[@]}"; do
     SECONDS=0
     export CLOUD_DEV_IMAGE=quay.io/mloriedo/cloud-dev-images:"${tag}"
-    echo ""
     echo -n "Testing editor startup in image ${CLOUD_DEV_IMAGE}..."
-    ./tests/test-editor-startup-in-image.sh  > ./tests/logs/"${tag}"_stdout.txt 2> ./tests/logs/"${tag}"_stderr.txt
-    duration=$SECONDS
-    echo "done in $duration sec."
+    if ./tests/test-editor-startup-in-image.sh  > ./tests/logs/"${tag}"_stdout.txt 2> ./tests/logs/"${tag}"_stderr.txt; then
+        duration=$SECONDS
+        echo "started in $duration sec."
+    else
+        duration=$SECONDS
+        echo "failed in $duration sec."
+    fi
 done
--- tests/test-editor-startup-in-image.sh
+++ tests/test-editor-startup-in-image.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 CLOUD_DEV_IMAGE=${CLOUD_DEV_IMAGE:-"quay.io/mloriedo/cloud-dev-images:ubi9"}
+EDITOR_IMAGE=${EDITOR_IMAGE:-"quay.io/che-incubator/che-code:insiders"}
 TEST_ID=$(printf "%03d" $((RANDOM % 1000)))
 echo "Testing CLOUD_DEV_IMAGE: ${CLOUD_DEV_IMAGE} / TEST_ID: ${TEST_ID}"
 
@@ -13,8 +14,25 @@ function cleanup {
     echo "##                                          ##"
     echo "##############################################"
     kubectl delete dw/dw-${TEST_ID}
+    echo ""
+    echo "##############################################"
+    echo "##                                          ##"
+    echo "##              DELETING DWT                ##"
+    echo "##                                          ##"
+    echo "##############################################"
+    kubectl delete dwt/editor
 }
 trap cleanup EXIT
+
+echo ""
+echo "##############################################"
+echo "##                                          ##"
+echo "##           CREATE EDITOR CR               ##"
+echo "##                                          ##"
+echo "##############################################"
+echo "EDITOR_IMAGE: ${EDITOR_IMAGE}"
+export EDITOR_CONTRIB=$(cat ./tests/editor-contribution.yaml | sed "s|IMAGE_NAME|${EDITOR_IMAGE}|g")
+kubectl apply -f - <<< "${EDITOR_CONTRIB}"
 
 echo ""
 echo "##############################################"
@@ -48,8 +66,13 @@ echo "##############################################"
 echo "##                                          ##"
 echo "##       READ VS CODE ENTRYPOINT LOGS       ##"
 echo "##           LOOK FOR STRING                ##"
-echo "##Listening and serving HTTP on 0.0.0.0:3333##"
+echo "##       \"Web UI available at ...\"        ##"
 echo "##                                          ##"
 echo "##############################################"
 export VS_CODE_LOGS=$(kubectl exec -it $POD -c dev-tooling -- /bin/bash -c "cat /checode/entrypoint-logs.txt")
-grep "Listening and serving HTTP on 0.0.0.0:333" <<< "${VS_CODE_LOGS}"
+if grep "Web UI available at http://localhost:3100/" <<< "${VS_CODE_LOGS}"; then
+    echo "SUCCESS: Found expected string in VS Code entrypoint logs"
+else
+    echo "FAILURE: Did not find expected string in VS Code entrypoint logs"
+    exit 1
+fi
